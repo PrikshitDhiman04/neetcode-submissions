a06064716f18c787b1e2c4ef9d4d6e05aff32aec
FakeChess <dhimanprikshit04@gmail.com>
2026-08-27 23:27:45 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    bool canEatAll(vector<int>& piles,int mid, int h ){
+        int actualHours = 0;
+
+        for( int &x : piles){
+            actualHours += x/mid;
+
+            if(x % mid != 0){
+                actualHours++;
+            }
+        }
+
+        return actualHours <= h;
+    }
+    
+
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+
+        int l = 1;
+        int r = *max_element(begin(piles), end(piles));
+
+        while(l < r){
+            int mid_hour = l + (r - l)/2;
+
+            if(canEatAll(piles, mid_hour, h)){
+                r = mid_hour;
+            }else{
+                l = mid_hour + 1;
+            }
+        }return l;
+    }
+};
