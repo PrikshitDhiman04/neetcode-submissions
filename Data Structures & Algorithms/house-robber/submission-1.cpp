0376759c4ef8219a59711d6d9ce7b04aeeae77cf
FakeChess <dhimanprikshit04@gmail.com>
2026-09-18 14:07:32 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:  // bottom up
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        vector<int> t(n + 1, 0);
+
+        t[0] = 0;
+        t[1] = nums[0];
+
+        for (int i = 2; i <= n; i++) {
+            int steal = nums[i - 1] + t[i - 2];
+            int skip = t[i - 1];
+
+            t[i] = max(skip, steal);
+        }
+
+        return t[n];
+    }
+};
